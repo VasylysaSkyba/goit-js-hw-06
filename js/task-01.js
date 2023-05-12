5a20245329f3218acf6d8a6e1225624567e4fdbe
Vasylysaskyba <vasylysaskyba@gmail.com>
2023-05-13 00:23:38 +0300

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,6 +1,6 @@
-const categories = document.querySelectorAll(".item");
-console.log(`Number of categories: ${categories.length}`);
-categories.forEach((category) => {
+const categoriesEl = document.querySelectorAll(".item");
+console.log(`Number of categories: ${categoriesEl.length}`);
+categoriesEl.forEach((category) => {
   console.log(`Category: ${category.firstElementChild.textContent}`);
   console.log(`Elements: ${category.lastElementChild.children.length}`);
 });
